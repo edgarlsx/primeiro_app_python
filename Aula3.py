c2ed2f6472b101dd43dfd2edc5505b179793099b
edgarlsx <edgarlsx@hotmail.com>
2020-06-20 22:30:59 -0300

    Aula 3 Python

---
+++ Aula3.py
@@ -0,0 +1,51 @@
+print('Validar notas do Aluno')
+
+a = int(input('Entre com o primeiro bimestre: '))
+if a > 10:
+    int(input('Voce digitou o numero errado. Deve ser menorm que 10: '))
+b = int(input('Entre com o segundo bimestre: '))
+if a > 10:
+    int(input('Voce digitou o numero errado. Deve ser menorm que 10: '))
+
+c = int(input('Entre com o terceiro bimestre: '))
+if a > 10:
+    int(input('Voce digitou o numero errado. Deve ser menorm que 10: '))
+d = int(input('Entre com o quarto bimestre: '))
+if a > 10:
+    int(input('Voce digitou o numero errado. Deve ser menorm que 10: '))
+
+media = (a + b + c + d) / 4
+
+print('Media é: {}'.format(media))
+
+
+
+#print('Vou informar se o numero é par ou impar')
+
+#a = int(input('Entre com o primeiro numero: '))
+#b = int(input('Entre com o segundo numero: '))
+
+#restoA = a % 2
+#restoB = b % 2
+
+#if restoA == 0 or restoB ==0 :
+#    print('Um Numero par foi digitado.')
+#else:
+#    print('Nenhum numero par foi digitado.')
+
+
+
+#print('Irei informar qual o maior numero informado.')
+
+#a = int(input('Entre com o primero número : '))
+#b = int(input('Entre com o segundo número : '))
+#c = int(input('Entre com o terceiro número : '))
+
+#if a > b and a >= c:
+#    print('O maior número é a {}'.format(a))
+#elif b > a and b > c:
+#    print('O maior numero é b {}'.format(b))
+#else:
+#    print('O Maior valor é c {}'.format(c))
+
+#print('Fim do Programa')
