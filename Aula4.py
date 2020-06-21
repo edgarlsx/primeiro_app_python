50763c93b9a311c98edf1be178ba23b9706c9e10
edgarlsx <edgarlsx@hotmail.com>
2020-06-20 23:04:24 -0300

    Aula 4

---
+++ Aula4.py
@@ -0,0 +1,35 @@
+# Comando For / While / Range
+
+print('Validar notas do Aluno')
+
+a = int(input('Entre com o primeiro bimestre: '))
+while a > 10:
+    a = int(input('Voce digitou a nota errada. Deve ser menor que 10: '))
+
+b = int(input('Entre com o segundo bimestre: '))
+while b > 10:
+    b = int(input('Voce digitou a nota errada. Deve ser menor que 10: '))
+
+c = int(input('Entre com o terceiro bimestre: '))
+while c > 10:
+    c = int(input('Voce digitou a nota errada. Deve ser menor que 10: '))
+
+d = int(input('Entre com o quarto bimestre: '))
+while d > 10:
+    d = int(input('Voce digitou a nota errada. Deve ser menor que 10: '))
+
+media = (a + b + c + d) / 4
+
+print('Media é: {}'.format(media))
+
+
+#print('Vou informar se o numero é primo.')
+
+#a = int(input('Entre com um numero: '))
+
+#for x in range (2, a+1):
+#    resto = a % x
+#    if resto == 0:
+#        print('O numero divide por {xx} portanto {aa} é primo.'.format(xx = x, aa=a))
+#    elif resto == 0 and resto <= a+1:
+#        print('O numero divide por {xx} portanto {aa} não é primo.'.format(xx = x, aa=a))
