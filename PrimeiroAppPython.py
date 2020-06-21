c9b59d996c1a859887b32aa96a90bcb8dfe2bee4
edgarlsx <edgarlsx@hotmail.com>
2020-06-20 21:37:00 -0300

    Aula 2 de Python

--- PrimeiroAppPython.py
+++ PrimeiroAppPython.py
@@ -1,1 +1,5 @@
 print ('Primeiro programa em python')
+a=2
+b=3
+soma = a+b
+print (soma)
---
+++ aula2.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5 
+soma = a + b
+
+subtracao = a - b
+
+multiplicacao = a * b
+
+divisao = a / b
+
+resto = a % b
+
+print('Soma: '+  str(soma))
+print( 'Soma: {som}, Subtração: {sub}, Multiplicação: {mul}, Divisao: {div}, Resto: {res}'.format(som=soma, sub = subtracao, mul=multiplicacao, div=divisao, res=resto))
